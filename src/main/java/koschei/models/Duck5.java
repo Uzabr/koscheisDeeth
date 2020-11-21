1def03cf261c023c7842cd39af8fc9894ce42d54
macBest <khaitboboev94@mail.ru>
2020-11-21 04:12:13 +0300

    beans are linked

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    private final Egg6 agge;
+@Autowired
+    public Duck5(Egg6 egg6) {
+        this.agge = egg6;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + "" + agge.toString();
     }
 }
